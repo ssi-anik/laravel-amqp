c98741bf89e9a5e613e073df6f7488942397a2a0
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-11-13 01:04:53 +0600

    facade method docblock added

--- src/Facades/Amqp.php
+++ src/Facades/Amqp.php
@@ -2,11 +2,15 @@
 
 namespace Anik\Laravel\Amqp\Facades;
 
+use Anik\Amqp\Exchanges\Exchange;
+use Anik\Amqp\Queues\Queue;
+use Anik\Amqp\Qos\Qos;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Anik\Laravel\Amqp\Amqp connection(?string $name = null)
- * @method static bool publish($messages, string $routingKey = '', ?Anik\Amqp\Exchanges\Exchange $exchange = null, array $options = []);
+ * @method static bool publish($messages, string $routingKey = '', ?Exchange $exchange = null, array $options = [])
+ * @method static void consume($handler, string $bindingKey = '', ?Exchange $exchange = null, ?Queue $queue = null, ?Qos $qos = null, array $options = [])
  */
 class Amqp extends Facade
 {
