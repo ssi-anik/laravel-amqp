d34144844d4f352520b7a76e07b3c1f7fd692380
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-11-13 11:05:31 +0600

    facade method annotation add, update

--- src/Facades/Amqp.php
+++ src/Facades/Amqp.php
@@ -3,17 +3,33 @@
 namespace Anik\Laravel\Amqp\Facades;
 
 use Anik\Amqp\Exchanges\Exchange;
-use Anik\Amqp\Queues\Queue;
 use Anik\Amqp\Qos\Qos;
+use Anik\Amqp\Queues\Queue;
+use Anik\Laravel\Amqp\AmqpFake;
+use Anik\Laravel\Amqp\AmqpPubSub;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Anik\Laravel\Amqp\Amqp connection(?string $name = null)
+ * @method static AmqpPubSub connection(?string $name = null)
  * @method static bool publish($messages, string $routingKey = '', ?Exchange $exchange = null, array $options = [])
  * @method static void consume($handler, string $bindingKey = '', ?Exchange $exchange = null, ?Queue $queue = null, ?Qos $qos = null, array $options = [])
+ *
+ * @method static void assertPublishedOnConnection(string $name)
+ * @method static void assertPublishedOnExchange(string $name)
+ * @method static void assertPublishedOnExchangeType(string $type)
+ * @method static void assertPublishedWithRoutingKey(string $key)
+ * @method static void assertPublished($message = null)
+ * @method static void assertNotPublished($message = null)
  */
 class Amqp extends Facade
 {
+    public static function fake(): AmqpPubSub
+    {
+        static::swap($fake = new AmqpFake(static::getFacadeApplication()));
+
+        return $fake;
+    }
+
     protected static function getFacadeAccessor(): string
     {
         return 'amqp';
